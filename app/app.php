915aaa88d7b383847b518f23d7166f34f395ca5a
Brittany Kerr and Nicole Freed <nicole@>
2017-06-26 14:50:19 -0700

    add package dimension entry form to app route /

--- app/app.php
+++ app/app.php
@@ -6,7 +6,42 @@
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Home";
+        return "
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+                <title>National Parcel Service</title>
+            </head>
+            <body>
+                <h1>Welcome to the National Parcel Service!</h1>
+                <h2>Enter the dimensions of your parcel below:</h2>
+                <form action='/parcel_calc'>
+                    <div class='form-group'>
+                        <label for='length'>Enter the package length: </label>
+                        <input type='number' name='length'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='width'>Enter the package width: </label>
+                        <input type='number' name='width'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='height'>Enter the package height: </label>
+                        <input type='number' name='height'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='weight'>Enter the package weight: </label>
+                        <input type='number' name='weight'>
+                    </div>
+                    <button type='submit' class='btn btn-primary' name='button'>Calculate my cost!</button>
+                </form>
+            </body>
+        </html>
+        ";
+    });
+
+    $app->get("/parcel_calc", function() {
+        return "The cost of your package is: ";
     });
 
     return $app;
